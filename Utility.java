bbe108ec5e43fb25b076e5f18261987519cd2f7b
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 15:55:57 +0530

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -1,1 +1,29 @@
+package com.bridgelab.utility;
 
+import java.util.Scanner;
+
+public class Utility {
+	Scanner scanner;
+	public Utility() {
+		scanner = new Scanner(System.in);
+	}
+	// get integer input
+	public int getIntValue() {
+		return scanner.nextInt();
+	}
+  //Fibonacci Series 
+	public static void fibonacci(int num1) {
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        int i = 1;
+        while (i<=num1-2) {
+            int c = a+b;
+            a = b;
+            b = c;
+            System.out.print(c + " ");
+            i++;
+        }
+	}
+}
