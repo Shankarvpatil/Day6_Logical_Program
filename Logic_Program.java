dffe993bc06988a1c3c2813dc2b8aa9d91e81fa1
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 16:30:45 +0530

    Update Logic_Program.java

--- Logic_Program.java
+++ Logic_Program.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-public class ReverseNum {
+public class ReverseNumber {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = s.nextInt();
 
-        int reverse = reverseNum(num);
+        int reverse = reverseNumber(num);
         System.out.println("Reverse number is: " + reverse);
     }
 
-    private static int reverseNum(int num) {
+    private static int reverseNumber(int num) {
         int rem = 0;
         int rev = 0;
         int temp = num;
