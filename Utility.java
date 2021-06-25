9ee2155cb0e289eb58172004a9f547eabd252ae8
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 16:08:09 +0530

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -1,1 +1,13 @@
+import java.util.Scanner;
 
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = s.nextInt();
+        int perfectNum = perfectNumber(num);
+        if (perfectNum == num)
+            System.out.println(num + " is a perfect number.");
+        else
+            System.out.println(num + " is not a perfect number.");
+    }
