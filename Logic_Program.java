2439b653a0a6a73f1b90dfaa539da5337b702248
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 16:29:41 +0530

    Update Logic_Program.java

--- Logic_Program.java
+++ Logic_Program.java
@@ -1,29 +1,25 @@
 import java.util.Scanner;
 
-public class PrimeNumber {
+public class ReverseNum {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int num = s.nextInt();
-        isPrime(num);
-    }
-    private static void isPrime(int num) {
-        int i = 2, count = 0;
-        if (num == 1 || num == 0) {
-            count++;
-            System.out.println(num + " is not a prime number.");
-        } else {
-            while(i<=num/2) {
-                if (num%i == 0) {
-                    count++;
-                }
-                i++;
-            }
-            if (count == 0)
-                System.out.println(num + " is prime number");
-            else
-                System.out.println(num + " is not a prime number.");
-        }
 
+        int reverse = reverseNum(num);
+        System.out.println("Reverse number is: " + reverse);
+    }
+
+    private static int reverseNum(int num) {
+        int rem = 0;
+        int rev = 0;
+        int temp = num;
+
+        while (temp > 0) {
+            rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp /10;
+        }
+        return rev;
     }
 }
