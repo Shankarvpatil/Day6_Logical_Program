b9c27c54cb823d920576a17f654cd63187e9c0a2
Shankar Vilas Patil <69981881+Shankarvpatil@users.noreply.github.com>
2021-06-25 16:26:32 +0530

    Update Logic_Program.java

--- Logic_Program.java
+++ Logic_Program.java
@@ -1,15 +1,26 @@
-package com.bridgelab.program;
+import java.util.Scanner;
 
-import com.bridgelab.utility.Utility;
-
-public class Fibbonacci_series {
-
-	public static void main(String[] args, int num1) {
-		Utility utility = new Utility();
-		System.out.print("Enter the number How many number of series you want: ");
-		int num1 = utility.getIntValue();
-		Utility.fibonacci(num1);
-
-	}
-
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = s.nextInt();
+        fibonacci(num);
+    }
+    private static void fibonacci(int num) {
+        int n = num;
+        int a = 0;
+        int b = 1;
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        int i = 1;
+        while (i<=num-2) {
+            int c = a+b;
+            a = b;
+            b = c;
+            System.out.print(c + " ");
+            i++;
+        }
+    }
 }
+
